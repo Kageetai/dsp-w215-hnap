61763a1296065494f10fd86fba0f10335e44a28d
Michael Seifarth <michael@getbynder.com>
2017-02-21 16:55:28 +0100

    add webpack.config.js and necessary modules

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+var webpack = require("webpack");
+
+module.exports = {
+    entry: './app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    }
+};
